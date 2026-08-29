8dc918d0a292d75a38702836cb1407d81e4ba478
Avinash Malhotra <avimalhotra505@gmail.com>
2026-08-29 14:12:41 +0530

    Next JS

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage(){
+     return (
+          <div className="container mx-auto">
+               <h1 className="text-5xl font-bold">About Us</h1>
+          </div>
+     )
+}
